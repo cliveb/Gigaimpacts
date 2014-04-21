3fa09659d7e8dfc7f750eafea4d8e76e8bb4d147
cliveb <clive.boulton@gmail.com>
2014-04-20 22:03:15 -0700

    map js from http to https

--- js/map.js
+++ js/map.js
@@ -11,7 +11,7 @@
     5
   ],
   "data": [
-    "http:\/\/a.tiles.mapbox.com\/v3\/kamini.WA_totalemissions\/markers.geojsonp"
+    "https:\/\/a.tiles.mapbox.com\/v3\/kamini.WA_totalemissions\/markers.geojsonp"
   ],
 
 
@@ -23,8 +23,8 @@
 */
 
   "grids": [
-    "http:\/\/a.tiles.mapbox.com\/v3\/kamini.WA_totalemissions\/{z}\/{x}\/{y}.grid.json",
-    "http:\/\/b.tiles.mapbox.com\/v3\/kamini.WA_totalemissions\/{z}\/{x}\/{y}.grid.json"
+    "https:\/\/a.tiles.mapbox.com\/v3\/kamini.WA_totalemissions\/{z}\/{x}\/{y}.grid.json",
+    "https:\/\/b.tiles.mapbox.com\/v3\/kamini.WA_totalemissions\/{z}\/{x}\/{y}.grid.json"
   ],
 
 
@@ -39,11 +39,11 @@
   "template": "{{#__location__}}{{\/__location__}}{{#__teaser__}}<body>\n<div align=\"center\">\n<h2 style=\"font-family:arial;font-size:12px;color:#484848;\">Zipcode: {{{ZCTA5CE10}}}<\/h2>\n<h3 style=\"font-family:arial;font-size:12px;color:#484848;\">{{{City}}}, {{{County}}} County, {{{State}}}<\/h3>\n<br>\n<h4 style=\"font-family:arial;font-size:25px;color:#484848;\">{{{e_simple}}}<\/h4>\n<h5 style=\"font-family:arial;font-size:13px;color:#484848;\">metric tons CO<sub>2<\/sub> equivalent<\/h5>\n\n<img src=\"http:\/\/chart.googleapis.com\/chart?\n&chxl=0:|T|H|F|G|S\n&chxp=a\n&chxr=0,0,4|1,0,30\n&chxs=0,676767,11.5,0,lt,676767\n&chxt=x,y\n&chyl=Transportation|Housing|Food|Goods|Services\n&chbh=a\n&chs=275x225\n&cht=bvg\n&chco=0033FF|990099|FF0066|FF6600|FFCC00|9933FF\n&chds=0,30\n&chd=t:{{{transporta}}},{{{Housing}}},{{{food}}},{{{goods}}},{{{services}}}\n&chdl=Transportation|Housing|Food|Goods|Services\n&chdlp=b|l\n&chma=20,20,20,20|25,75\n&chts=676767,12\" width=\"220\" height=\"200\"\/>\n<\/div>\n<\/body>\n{{\/__teaser__}}{{#__full__}}{{\/__full__}}",
   "tilejson": "2.0.0",
   "tiles": [
-    "http:\/\/a.tiles.mapbox.com\/v3\/kamini.WA_totalemissions\/{z}\/{x}\/{y}.png",
-    "http:\/\/b.tiles.mapbox.com\/v3\/kamini.WA_totalemissions\/{z}\/{x}\/{y}.png"
+    "https:\/\/a.tiles.mapbox.com\/v3\/kamini.WA_totalemissions\/{z}\/{x}\/{y}.png",
+    "https:\/\/b.tiles.mapbox.com\/v3\/kamini.WA_totalemissions\/{z}\/{x}\/{y}.png"
   ],
   "version": "1.0.0",
-  "webpage": "http:\/\/a.tiles.mapbox.com\/v3\/kamini.WA_totalemissions\/page.html",
+  "webpage": "https:\/\/a.tiles.mapbox.com\/v3\/kamini.WA_totalemissions\/page.html",
   
 
     "options": [
