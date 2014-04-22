affe68a5400f8d786f46192265232b1a7ee3a880
cliveb <clive.boulton@gmail.com>
2014-04-22 04:39:32 -0700

    accreditation

--- js/map.js
+++ js/map.js
@@ -77,6 +77,6 @@
 
   window.account = {
   "id": "kamini",
-  "name": "CoolClimate Network",
+  "name": "Giga Impacts",
   "avatar": "https:\/\/www.gravatar.com\/avatar\/de3b099b1452612f50070efc1edb89b9.jpg?d=mm"
 };
