13fd68c912a9c4bb2b02a8ea4c43b743c1093ae4
cliveb <clive.boulton@gmail.com>
2014-04-22 04:35:41 -0700

    revert

--- js/map.js
+++ js/map.js
@@ -78,5 +78,5 @@
   window.account = {
   "id": "kamini",
   "name": "CoolClimate Network",
-  /* "avatar": "https:\/\/www.gravatar.com\/avatar\/de3b099b1452612f50070efc1edb89b9.jpg?d=mm" */
+  "avatar": "https:\/\/www.gravatar.com\/avatar\/de3b099b1452612f50070efc1edb89b9.jpg?d=mm"
 };
